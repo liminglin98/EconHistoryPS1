48cd52751a442acfbdab2346511cdece1b947579
Liming Lin <liminglin1998@gmail.com>
2025-10-24 00:04:04 +0200

    just a start

--- Assignment1.r
+++
@@ -1,8 +0,0 @@
-# Load common R packages
-library(tidyverse)  # For data manipulation and visualization
-library(data.table) # For fast data manipulation
-library(readr)      # For reading data
-library(dplyr)      # For data manipulation
-library(ggplot2)    # For data visualization
-library(stringr)    # For string manipulation
-library(lubridate)  # For working with dates and times
